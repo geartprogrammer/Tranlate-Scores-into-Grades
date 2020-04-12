2d1b4723f45ac4e03abcbf2e25baa3863325619f
Geart Ferhati <55233184+geartprogrammer@users.noreply.github.com>
2020-04-12 18:22:01 +0200

    main.py
    Added if statements and the input d

---
+++ main.py
@@ -0,0 +1,19 @@
+score = int(input('Enter your score: '))
+
+if score == 100:
+  print('Perfect score!')
+
+if score >= 90 and score < 100:
+  print('A')
+   
+if score >= 80 and score < 90:
+  print('B')
+
+if score >= 70 and score < 80:
+  print('C')
+
+if score >=60 and score < 70:
+  print('D')
+
+if score < 60:
+  print('F')
